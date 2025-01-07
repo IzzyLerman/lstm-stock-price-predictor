84676106563ccbe6922d6ca5a01bfbb20caef9b9
IzzyLerman <izzylerman14@gmail.com>
2025-01-07 09:53:49 -0800

    data preprocessing

--- train_model.py
+++ train_model.py
@@ -16,13 +16,28 @@ def load_data() -> np.array:
     returns: list of t nx9 ndarrays
     '''
     start_date = '20210101'
+    data = []
     psql_password = os.getenv('POSTGRES_PASSWORD')
     conn = psycopg2.connect(database='postgres',host='localhost',user='postgres',password=psql_password,port='5432')
     cur = conn.cursor()
+    
     try:
         cur.execute('SELECT root FROM features WHERE date = %s;',(start_date,))
         roots = cur.fetchall()
         roots = [root[0] for root in roots]
+        cur.execute('SELECT MIN(date) FROM features;')
+        start_date = cur.fetchone()[0]
+        print(start_date)
+        cur.execute('SELECT MAX(date) FROM features;')
+        end_date = cur.fetchone()[0]
+        print(end_date)
+
+        '''
+        for root in roots:
+            cur.execute('SELECT ')
+        '''
+        
+        
     except Exception as e:
         print(e)
         conn.rollback()
@@ -32,4 +47,4 @@ def load_data() -> np.array:
 
 if __name__ == '__main__':
     load_data()
-
+    
